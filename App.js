2d612bc2355e5401c1816bf1ca29ed9a87fad3c2
Verah Nyagaka <verahnyagaka101@gmail.com>
2022-12-10 23:29:59 +0300

    updated

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, View, Text } from 'react-native';
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { createAppContainer } from "react-navigation";
+import { createMaterialTopTabNavigator } from "react-navigation-tabs";
+
+import ProfileScreen from "./screens/ProfileScreen";
+import ImagesScreen from "./screens/ImagesScreen";
+import VideoScreen from "./screens/VideosScreen";
+
+const TabNavigator = createMaterialTopTabNavigator(
+{
+	Profile: {
+	screen: ProfileScreen,
+	navigationOptions: {
+		tabBarLabel: "Profile",
+		showLabel: ({ focused }) => {
+		console.log(focused);
+		return focused ? true : false;
+		},
+		tabBarIcon: (tabInfo) => (
+		<Ionicons
+			name="ios-person-circle-outline"
+			size={tabInfo.focused ? 25 : 20}
+			color={tabInfo.tintColor}
+		/>
+		),
+	},
+	},
+	Images: {
+	screen: ImagesScreen,
+	navigationOptions: {
+		tabBarLabel: "Images",
+		tabBarIcon: (tabInfo) => (
+		<Ionicons
+			name="ios-images-outline"
+			size={tabInfo.focused ? 24 : 20}
+			color={tabInfo.tintColor}
+		/>
+		),
+	},
+	},
+	Video: {
+	screen: VideoScreen,
+	navigationOptions: {
+		tabBarLabel: "Videos",
+		tabBarIcon: (tabInfo) => (
+		<Ionicons
+			name="ios-videocam-outline"
+			size={tabInfo.focused ? 25 : 20}
+			color={tabInfo.tintColor}
+		/>
+		),
+	},
+	},
+},
+{
+	tabBarOptions: {
+	showIcon: true,
+
+	style: {
+		backgroundColor: "#006600",
+		marginTop: 28,
+	},
+	},
+}
+);
+
+const Navigator = createAppContainer(TabNavigator);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+return (
+	<Navigator>
+	<ProfileScreen />
+	</Navigator>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
